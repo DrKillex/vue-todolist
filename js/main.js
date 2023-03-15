b36ef5a2c71269872e58b1cc7db497eaa74b5347
DrKillex <lcollaro1994@gmail.com>
2023-03-15 15:29:58 +0100

    FEAT milestone 1

--- js/main.js
+++ js/main.js
@@ -3,9 +3,22 @@
 const { createApp } = Vue
 
 createApp({
-  data() {
-    return {
-      
+    data() {
+        return {
+            todoList: [
+                {
+                    text: 'testo 1',
+                    done: false
+                },
+                {
+                    text: 'testo 2',
+                    done: true
+                }
+            ],
+            newTodo: {
+                text: '',
+                done: false
+            },
+        }
     }
-  }
 }).mount('#app')
